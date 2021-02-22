e8d0d7c59694d232ef2f2b2d59998f0c764e3102
nikita <sarodenikita24@gmail.com>
2021-02-22 16:35:52 +0530

    first commit

---
+++ work.js
@@ -0,0 +1,84 @@
+// all the images
+var back_images = ["img/1.png", "img/1.png", "img/2.png", "img/2.png", "img/3.png", "img/3.png", "img/4.png", "img/4.png", "img/5.png", "img/5.png", ]
+
+let card = document.getElementsByClassName("card");
+let cards = [...card];
+
+// collects all the 
+const deck = document.getElementsByClassName("back");
+
+ // array for opened cards
+var openedCards = [];
+
+// window reload on startGame and call shuffle card
+document.body.onload = startGame();
+function startGame(){
+	var shuffledCards = Shuffle(back_images);
+	console.log(shuffledCards);
+	for (var i = deck.length - 1; i >= 0; i--) {
+		deck[i].src = shuffledCards[i];
+	}
+}
+
+// loop to add event listener to each card
+for (var i = cards.length - 1; i >= 0; i--) {
+	cards[i].addEventListener("click", displayCard);
+}
+
+// to display the card
+function displayCard(){
+	var child = this.childNodes;
+	child[1].classList.add("front");
+	child[3].classList.remove("back");
+
+	// adding opened card to an array
+	openedCards.push(this);
+	console.log(openedCards);
+	if(openedCards.length == 2){
+		openedCard();
+	}
+}
+
+// Fisher-Yates (aka Knuth) shuffle 
+function Shuffle(Array){
+	var currentIndex = Array.length, tempValue, randIndex;
+
+	while(currentIndex != 0){
+		randIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		tempValue = Array[currentIndex];
+		Array[currentIndex] = Array[randIndex];
+		Array[randIndex] = tempValue;
+	}
+	return Array;
+}
+
+// to check if matched
+function openedCard(){
+	var len = openedCards.length;
+	if (len == 2) {
+		if (openedCards[0].children[1].src == openedCards[1].children[1].src) {
+			matched();
+		}else{
+			unmatched();
+		}
+	}
+}
+
+function matched(){
+	openedCards[0].style.boxShadow = "none";
+
+	openedCards[1].style.boxShadow = "none";
+
+	openedCards = [];
+}
+
+function unmatched(){
+	openedCards[0].children[0].classList.remove("front");
+	openedCards[0].children[1].classList.add("back");
+
+	openedCards[1].children[0].classList.remove("front");
+	openedCards[1].children[1].classList.add("back");
+
+	openedCards = [];
+}
